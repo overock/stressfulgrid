3c04d7ed718dcf2daf025ef61791bff1257481d0
Roaster Kay <overock@ex-em.com>
2017-03-30 18:32:51 +0900

    뭔가 더 꼬인 느낌 -_-;; 템플릿 분리 시도

---
+++ templates/grid.vue
@@ -0,0 +1,18 @@
+<table @wheel.prevent="scroll">
+  <thead>
+    <tr>
+      <th v-for="k in columns">
+        {{k}}
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="e in filteredData">
+      <td v-for="k in columns">
+        <span v-if="k[0]=='n' && e[k]>90" style="color:#f00;">{{e[k]}}</span>
+        <span v-else-if="k[0]=='n' && e[k]>70" style="color:#aa0;">{{e[k]}}</span>
+        <span v-else>{{e[k]}}</span>
+      </td>
+    </tr>
+  </tbody>
+</table>
