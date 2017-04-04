65c4525d72857ff65ec20ca3d5c943a5678b015a
Roaster Kay <overock@ex-em.com>
2017-04-04 13:20:15 +0900

    Markup 작업

--- scripts/gridtest.js
+++ scripts/gridtest.js
@@ -4,15 +4,26 @@ document.addEventListener('DOMContentLoaded', function() {
     props: {
       data: Array,
       columns: Array,
+      widths: Array,
       filterKey: String,
       rowIndex: Number
     },
     computed: {
       filteredData: function() {
+        console.log(this.widths);
         var $this = this;
-        var ret = this.data.filter(function(v, i) { return v.n01>=50; })
-                           .filter(function(v, i) { return i>=$this.rowIndex && i<$this.rowIndex+10; });
-        return ret;
+        return this.data.filter(function(v, i) { return v.n01>=50; });
+      },
+      windowed: function() {
+        var $this = this;
+        return this.filteredData.filter(function(v, i) { return i>=$this.rowIndex && i<$this.rowIndex+30; });
+      },
+      summary: function() {
+        this.filteredData.reduce(function(p, c, i) {
+          if(!p) return c;
+          
+          return 
+        });
       }
     },
     methods: {
@@ -32,12 +43,14 @@ document.addEventListener('DOMContentLoaded', function() {
     data: {
       searchQuery: '',
       gridData: testData,
-      gridColumn: [ 'lastName','firstName','address','zipCode','email','phone','company','catchPhrase','signature',
-                    'n01','n02','n03','n04','n05','n06','n07','n08','n09','n10',
-                    'n11','n12','n13','n14','n15','n16','n17','n18','n19','n20',
-                    'n21','n22','n23','n24','n25','n26','n27','n28','n29','n30',
-                    'n31','n32','n33','n34','n35','n36','n37','n38','n39','n40',
-                    'n41','n42','n43','n44','n45','n46','n47','n48','n49','n50' ],
+//      gridColumn: [ 'lastName','firstName','address','zipCode','email','phone','company','catchPhrase','signature',
+//                    'n01','n02','n03','n04','n05','n06','n07','n08','n09','n10',
+//                    'n11','n12','n13','n14','n15','n16','n17','n18','n19','n20',
+//                    'n21','n22','n23','n24','n25','n26','n27','n28','n29','n30',
+//                    'n31','n32','n33','n34','n35','n36','n37','n38','n39','n40',
+//                    'n41','n42','n43','n44','n45','n46','n47','n48','n49','n50' ],
+      gridColumn: [ 'lastName','firstName', 'zipCode','email','phone','company', 'n01','n02','n03','n04','n05','n06','n07','n08','n09','n10' ],
+      gridWidths: [ 100, 100, 100, 300, 200, 300, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40],
       gridIndex: 0
     },
     mounted() {
--- templates/grid.vue
+++ templates/grid.vue
@@ -1,13 +1,13 @@
 <table @wheel.prevent="scroll">
   <thead>
     <tr>
-      <th v-for="k in columns">
-        {{k}}
+      <th v-for="(v, k, i) in columns" v-bind:width="widths[k]">
+        {{v}}
       </th>
     </tr>
   </thead>
   <tbody>
-    <tr v-for="e in filteredData">
+    <tr v-for="e in windowed">
       <td v-for="k in columns">
         <span v-if="k[0]=='n' && e[k]>90" style="color:#f00;">{{e[k]}}</span>
         <span v-else-if="k[0]=='n' && e[k]>70" style="color:#aa0;">{{e[k]}}</span>
@@ -15,4 +15,11 @@
       </td>
     </tr>
   </tbody>
+  <tfoot>
+    <tr>
+      <td v-for="v in columns">
+        
+      </td>
+    </tr>
+  </tfoot>
 </table>
